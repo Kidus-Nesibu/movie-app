46885f3dd7b18aa579a27f46fa01d10f54cc12bf
Kidus Nesibu <knesibu3@gmail.com>
2026-08-24 15:52:29 +0300

    setup registration end point

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -128,4 +128,17 @@ MAILERS = {
     },
 }
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+}
+
 AUTH_USER_MODEL = "accounts.User"
