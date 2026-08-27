42d5df293d58272c7279f629c08d75697ab76dbd
Kidus Nesibu <knesibu3@gmail.com>
2026-08-27 13:39:15 +0300

    set up jwt auth and token blacklist

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
 
-from .views import RegisterView
+from rest_framework_simplejwt.views import(
+        TokenObtainPairView,
+        TokenObtainPairView,
+        TokenRefreshView,
+        )
+
+
+from .views import RegisterView, MeView
 
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("me/", MeView.as_view(), name="me"),
+
 ]
